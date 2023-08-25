841566644bc9d2ee335566591186d372bb57b5ae
rafaelmorenov <santiagomoreno124@gmail.com>
2023-08-24 20:14:24 -0500

    se agrega el proyecto

---
+++ app2.0/app2.0/app2_0App.swift
@@ -0,0 +1,18 @@
+//
+//  app2_0App.swift
+//  app2.0
+//
+//  Created by Rafael Santiago Moreno Velasquez on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct app2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
